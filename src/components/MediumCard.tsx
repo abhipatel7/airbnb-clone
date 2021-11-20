eaa6bee00af954b58aa71fc84191b52746c57f26
abhipatel7 <abhipatel7420@gmail.com>
2021-11-20 16:35:56 +0530

    main section of homepage created

---
+++ src/components/MediumCard.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+import { ICardData } from '../interfaces';
+
+interface Props {
+  card: ICardData;
+}
+
+const MediumCard: FC<Props> = ({ card: { img, title } }) => {
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80">
+        <Image src={img} layout="fill" className="rounded-xl" />
+      </div>
+
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+};
+
+export default MediumCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
 
-const Home: NextPage = () => {
+import { ICardData, IExploreData } from '../interfaces';
+
+interface Props {
+  exploreData: IExploreData[];
+  cardsData: ICardData[];
+}
+
+const Home: NextPage<Props> = ({ exploreData, cardsData }) => {
   return (
     <div>
       <Head>
@@ -15,8 +25,54 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
-    </div>
-  )
-}
 
-export default Home
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((data: IExploreData) => (
+              <SmallCard key={data.location} data={data} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((card: ICardData) => (
+              <MediumCard key={card.title} card={card} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="/large-banner.webp"
+          title="The Greatest Outdoors"
+          description="Whislits curated by Airbnb."
+          buttonText="Get Inspired."
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const exploreData = await fetch('https://jsonkeeper.com/b/4G1G').then((res) =>
+    res.json()
+  );
+
+  const cardsData = await fetch('https://jsonkeeper.com/b/VHHT').then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+};
